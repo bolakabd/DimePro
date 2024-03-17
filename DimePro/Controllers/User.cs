b37841a7252f010895dbd9be9937622a3c420380
Tanzir Riad <thriad@gmail.com>
2024-03-16 23:29:08 -0400

    Initial change

---
+++ DimePro/Controllers/User.cs
@@ -0,0 +1,51 @@
+﻿using DimePro.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DimePro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        // GET: api/<User>
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> Get()
+        {
+            List<AppUser> users = new List<AppUser>();
+            users.Add(new AppUser(1,"Riad"));
+            users.Add(new AppUser(2, "Mark"));
+
+            return users;
+            //return new string[] { "Riad", "Mark" };
+        }
+
+        // GET api/<User>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id < 5)
+                return "Riad";
+            return "Mark";
+        }
+
+        // POST api/<User>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<User>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<User>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DimePro/Entity/AppUser.cs
@@ -0,0 +1,14 @@
+﻿namespace DimePro.Entity
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public AppUser(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
